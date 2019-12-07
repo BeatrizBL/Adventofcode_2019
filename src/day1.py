e204e52496476ec4c18200469c921c44dc83a421
Beatriz <beatriz.bueno.larraz@gmail.com>
2019-12-07 14:21:52 +0100

    Day 1 - part 2

--- src/day1.py
+++ src/day1.py
@@ -29,3 +29,42 @@ def compute_fuel_requirements(mass_file: str,
     fuel = [(math.floor(m/3)-2) for m in mass]
 
     return sum(fuel)
+
+
+def compute_fuel_mass(mass: int) -> int:
+    """
+    Computes the fuel required for a specific mass, taking
+    into account that the fuel has mass itself.
+    """
+    # Stopping condition
+    if mass <= 0:
+        return 0
+
+    fuel = (math.floor(mass/3)-2)
+    fuel = fuel if fuel > 0 else 0
+
+    return fuel + compute_fuel_mass(fuel)
+
+
+def compute_all_fuel_requirements(mass_file: str,
+                                  data_path: str = 'data/raw/day1',
+                                  ) -> int:
+    """
+    Computes what is the sum of the fuel requirements for all the 
+    modules on your spacecraft. Taking into account the mass of 
+    the added fuel.
+    """
+
+    # Read the file
+    file_path = os.path.join(data_path, mass_file)
+    df_mass = pd.read_csv(file_path)
+
+    # Check the content
+    if len(df_mass.columns) > 1:
+        raise ValueError('One column expected!')
+    mass_col = list(df_mass.columns)[0]
+
+    mass = list(df_mass[mass_col].values)
+    fuel = [compute_fuel_mass(m) for m in mass]
+
+    return sum(fuel)
