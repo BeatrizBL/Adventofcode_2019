04bc4f9b3a823897cfc5aa1a14623a64c053db48
Beatriz <beatriz.bueno.larraz@gmail.com>
2020-01-03 00:07:07 +0100

    Day 10 - part 2

--- src/day10.py
+++ src/day10.py
@@ -1,8 +1,12 @@
 from typing import List
+from queue import Queue
 import numpy as np
 import pandas as pd
+import math
 import os
 
+import ipywidgets as widgets
+from ipywidgets import interact
 from src.visualization import altair_plots
 
 
@@ -69,6 +73,52 @@ def _find_close(inline: List[tuple],
     return closest
 
 
+def _find_inline_asteroids(asteroids_map: np.ndarray,
+                           pos: tuple,
+                           asteroid: tuple,
+                           one_side: bool = False
+                           ) -> List[tuple]:
+
+    x, y = asteroid
+
+    # Posible positions
+    x_positions = list(range(asteroids_map.shape[0]))
+    y_positions = list(range(asteroids_map.shape[1]))
+
+    # Just one side of the line
+    if one_side is True:
+        if pos[0] != x:
+            # Non vertical line
+            if pos[0] > x:
+                x_positions = [
+                    a for a in x_positions if a < pos[0]]  # Pos right
+            else:
+                x_positions = [
+                    a for a in x_positions if a > pos[0]]  # Pos left
+        else:
+            # Vertical line
+            if pos[1] > y:
+                y_positions = [
+                    b for b in y_positions if b < pos[1]]  # Pos down
+            else:
+                y_positions = [b for b in y_positions if b > pos[1]]  # Pos up
+
+    # Compute the positions in the same line
+    def line(pos_x): return y + (pos_x-x)*(pos[1]-y)/(pos[0]-x)
+    if pos[0] != x:
+        values = [(a, int(line(a)))
+                  for a in x_positions if line(a).is_integer()]
+    else:
+        values = [(x, b) for b in y_positions]
+
+    # Remove positions out of the bounds
+    values = [p for p in values if p[1] >=
+              0 and p[1] < asteroids_map.shape[1]]
+
+    # Filter to asteroids in those positions
+    return([p for p in values if asteroids_map[p[0], p[1]] > 0])
+
+
 def mark_visible_asteroids(asteroids_map: np.ndarray,
                            pos: tuple
                            ) -> np.ndarray:
@@ -81,31 +131,15 @@ def mark_visible_asteroids(asteroids_map: np.ndarray,
     asteroids_map = asteroids_map.copy()
     asteroids_map[pos[0], pos[1]] = 0
 
-    # Posible positions
-    x_positions = list(range(asteroids_map.shape[0]))
-    y_positions = list(range(asteroids_map.shape[1]))
-
     # Get list of unchecked asteroids
     unchecked = list(zip(*np.where(asteroids_map == 1)))
 
-    while len(unchecked)>0:
-        x,y = unchecked[0]
+    while len(unchecked) > 0:
+        asteroid = unchecked[0]
 
         # Compute the positions in the same line
-        def line(pos_x): return y + (pos_x-x)*(pos[1]-y)/(pos[0]-x)
-        if pos[0] != x:
-            values = [(a, int(line(a)))
-                      for a in x_positions if line(a).is_integer()]
-        else:
-            values = [(x, b) for b in y_positions]
-
-        # Remove positions out of the bounds
-        values = [p for p in values if p[1] >=
-                  0 and p[1] < asteroids_map.shape[1]]
-
-        # Filter to asteroids in those positions
-        inline_asteroids = [
-            p for p in values if asteroids_map[p[0], p[1]] > 0]
+        inline_asteroids = _find_inline_asteroids(asteroids_map,
+                                                  pos, asteroid)
 
         # Keep just the two closest asteroids
         closest_asteroids = _find_close(inline_asteroids, pos)
@@ -119,7 +153,10 @@ def mark_visible_asteroids(asteroids_map: np.ndarray,
 
 def plot_asteroid_map(asteroids_map: np.ndarray,
                       visible_asteroids: np.ndarray,
-                      optimal_point: tuple
+                      optimal_point: tuple,
+                      legend_visible: bool = True,
+                      xlims: tuple = None,
+                      ylims: tuple = None
                       ):
     """
     Plot an asteroid map, marking the visible and non-visible ones
@@ -139,7 +176,10 @@ def plot_asteroid_map(asteroids_map: np.ndarray,
 
     # Plot
     altair_plots.scatter_plot(df,
-                              sorting=['Optimal', 'Visible', 'Non visible']
+                              sorting=['Optimal', 'Visible', 'Non visible'],
+                              legend_visible=legend_visible,
+                              xlims=xlims,
+                              ylims=ylims
                               ).display()
 
 
@@ -170,4 +210,114 @@ def count_optimal_detected(asteroids_map: np.ndarray) -> int:
     # Plot the asteroid map
     plot_asteroid_map(asteroids_map, visible_asteroids, max_position)
 
-    return max_visible
+    return (max_position, max_visible)
+
+
+def build_angle_structure(asteroids_map: np.ndarray,
+                          location: tuple
+                          ) -> dict:
+    """
+    Creates a dictionary structure to store the asteroids aligned in a 
+    given angle, when comparing with the vertical line from the provided
+    location.
+    """
+
+    # Get list of unchecked asteroids
+    unchecked = list(zip(*np.where(asteroids_map == 1)))
+    unchecked.remove(location)
+    angles = {}
+
+    while len(unchecked) > 0:
+        asteroid = unchecked[0]
+
+        # Find other asteroids in the same direction
+        aligned = _find_inline_asteroids(asteroids_map,
+                                         location, asteroid,
+                                         one_side=True)
+
+        # Sort them by distance
+        ds = [(location[0]-a)**2+(location[1]-b)**2 for a, b in aligned]
+        aligned_sorted = [aligned[ix] for ix in np.argsort(ds)]
+
+        # Compute the angle to the vertical
+        v = (asteroid[0]-location[0], asteroid[1]-location[1])
+        dot_product = sum((a*b) for a, b in zip(v, (0, -1)))
+        norm = math.sqrt(sum((a*b) for a, b in zip(v, v)))
+        angle = round(math.degrees(math.acos(dot_product / norm)), 2)
+        if asteroid[0] < location[0]:
+            angle = 360 - angle
+
+        # Store the asteroids
+        angles[angle] = aligned_sorted
+        [unchecked.remove(ast) for ast in aligned]
+
+    return angles
+
+
+def vaporize_asteroids(asteroids_map: np.ndarray,
+                       location: tuple,
+                       n_asteroids: int = None
+                       ) -> List[tuple]:
+    """
+    Computes the vaporized asteroids until a desired number. That is,
+    the first visible asteroid each round when rotating from the up
+    vertical position.
+    Returns a list of the asteroids in order.
+    """
+
+    # Final number of asteroids to vaporize
+    total = asteroids_map.sum() - 1
+    if n_asteroids is None or n_asteroids > total:
+        n_asteroids = total
+
+    # Get angles
+    angles = build_angle_structure(asteroids_map, location)
+    rotation_angles = sorted(list(angles.keys()))
+    rotation = Queue()
+    [rotation.put(a) for a in rotation_angles]
+
+    vaporized = []
+    for i in range(n_asteroids):
+        angle = rotation.get()
+        asteroids = angles[angle]
+        vaporized.append(asteroids[0])
+
+        # If there are more asteroids, add the angle back
+        if len(asteroids) > 1:
+            angles[angle] = asteroids[1:]
+            rotation.put(angle)
+        else:
+            angles[angle] = []
+
+    return vaporized
+
+
+def animated_vaporization(asteroids_map: np.ndarray,
+                          location: tuple,
+                          vaporized: List[tuple]):
+    """
+    Returns an interactive plot highlighting and removing the
+    vaporized asteroids in order.
+    """
+
+    asteroids_map = asteroids_map.copy()
+
+    def demo(i):
+        asteroid = vaporized[i]
+        fire = np.zeros(asteroids_map.shape)
+        fire[asteroid[0], asteroid[1]] = 1
+        chart = plot_asteroid_map(asteroids_map,
+                                  visible_asteroids=fire,
+                                  optimal_point=location,
+                                  legend_visible=False,
+                                  xlims=(0, asteroids_map.shape[0]-1),
+                                  ylims=(-asteroids_map.shape[1]+1, 0),
+                                  )
+        asteroids_map[asteroid[0], asteroid[1]] = 0
+        return chart
+
+    return interact(demo, i=widgets.Play(value=0,
+                                         min=0,
+                                         max=(len(vaporized)-1),
+                                         step=1,
+                                         ))
--- src/visualization/altair_plots.py
+++ src/visualization/altair_plots.py
@@ -21,22 +21,37 @@ def scatter_plot(data: pd.DataFrame,
                  y_column: str = 'y',
                  color_column: str = 'value',
                  size: int = None,
-                 sorting = None
+                 sorting = None,
+                 legend_visible: bool = True,
+                 xlims: tuple = None,
+                 ylims: tuple = None
                  ):
 
     # Coloring column
     if sorting is not None:
-        color = alt.Color(color_column+':N', sort=sorting)
+        if legend_visible is True:
+            color = alt.Color(color_column+':N', sort=sorting)
+        else:
+            color = alt.Color(color_column+':N', sort=sorting, legend=None)
     else:
-        color = alt.Color(color_column+':N')
+        if legend_visible is True:
+            color = alt.Color(color_column+':N')
+        else:
+            color = alt.Color(color_column+':N', legend=None)
 
     # Size of the dots
     if size is None:
         size = 5000/max(data[x_column].max(), data[y_column].max())
 
+    # Axis limits
+    if xlims is None:
+        xlims = (int(data[x_column].min()), int(data[x_column].max()))
+    if ylims is None:
+        ylims = (int(data[y_column].min()), int(data[y_column].max()))
+
     chart = alt.Chart(data).mark_circle(size=size).encode(
-        x=x_column+':Q',
-        y=y_column+':Q',
+        x=alt.X(x_column+':Q', scale=alt.Scale(domain=(xlims[0], xlims[1]))),
+        y=alt.Y(y_column+':Q', scale=alt.Scale(domain=(ylims[0], ylims[1]))),
         color=color
     )
     return chart
