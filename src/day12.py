86bc128e54a76759d5d51247a9d2882ec03c63f5
Beatriz <beatriz.bueno.larraz@gmail.com>
2020-01-25 13:30:56 +0100

    Day 12 - part 2

--- src/day12.py
+++ src/day12.py
@@ -7,6 +7,10 @@ import matplotlib.animation
 import matplotlib.cm as cm
 from mpl_toolkits.mplot3d import Axes3D
 
+#################################################################
+# PART 1                                                        #
+# Inefficient implementation based on DataFrame transformations #
+#################################################################
 
 def update_velocity(positions: pd.DataFrame,
                     velocities: pd.DataFrame
@@ -116,3 +120,64 @@ def animate_system(positions: pd.DataFrame,
                                              interval=200,
                                              repeat=False)
     return ani
+
+
+################################################################
+# PART 2                                                       #
+# More efficient implementation based on the thoughts:         #
+#                                                              #
+#  - If the same positions and velocities are reach, the same  #
+#    behavior would be repited -> We are inside a cicle        #
+#                                                              #
+#  - If we are in a cicle, the first state to be repeated is   #
+#    the current state -> No need to store intermediate states #
+#                                                              #
+#  - The 3 dimensions are independent from each other -> The   #
+#    first repetition would be the Least Common Multiple of    #
+#    the cicle lengths for each dimension                      #
+################################################################
+
+
+def cicle_length(positions: pd.DataFrame) -> int:
+    """
+    Computes the number of steps until the first state repetition
+    """
+
+    # Initialize velocities
+    velocities = pd.DataFrame({'x': [0]*len(positions),
+                               'y': [0]*len(positions),
+                               'z': [0]*len(positions)})
+
+    # Store first state
+    first_pos = positions.copy()
+    first_vel = velocities.copy()
+
+    # Loop over each dimension
+    cicles = []
+    for dim in velocities.keys():
+        print(f'Dimension: {dim}')
+
+        # Initialize 
+        steps = 0
+        reached_initial = False
+        pos = first_pos[dim].values
+        vel = first_vel[dim].values
+
+        while not reached_initial:
+            steps += 1
+
+            if steps % 1000 == 0:
+                print(steps)
+
+            # Apply gravity
+            vel_change = [sum([(1 if x<y else -1) for y in [p for p in pos if p!=x]]) for x in pos]
+            vel = vel + vel_change
+
+            # Apply velocity
+            pos = pos + vel
+
+            if all(first_pos[dim].values == pos) & all(first_vel[dim].values == vel):
+                cicles.append(steps)
+                reached_initial = True
+
+    return np.lcm.reduce(cicles)
