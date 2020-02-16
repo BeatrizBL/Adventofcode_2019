2113a84345b06cc6eeff3efe87003281a8c14254
Beatriz <beatriz.bueno.larraz@gmail.com>
2020-02-16 18:11:20 +0100

    Day 13, part 2, full game run

--- src/day13.py
+++ src/day13.py
@@ -19,8 +19,8 @@ class ArcadeCabinet(object):
 
     def __init__(self, program_path: str,
                  root_path: str = 'data/raw',
-                 console: bool = False,
-                 autoplay: bool = False
+                 autoplay: bool = False,
+                 print_screen: bool = True
                  ):
 
         # Check the parameters
@@ -44,12 +44,15 @@ class ArcadeCabinet(object):
         self.screen = None
 
         # Game parameters
-        self.console = console
+        self.console = False
         self.score = 0
+        self.max_score = 0  # To print when game is over
         self.autoplay = autoplay
+        self.print_screen = print_screen
 
     def insert_quarters(self, quarters: int):
         self.computer.update_memory((0, quarters))
+        self.console = True
 
     def _update_status(self, output: List[int]):
         """ Update screen using the received computer output """
@@ -61,6 +64,8 @@ class ArcadeCabinet(object):
                 if output[i+1] != 0:
                     raise ValueError('Incorrect output format!', output)
                 self.score = output[i+2]
+                if self.score > self.max_score:
+                    self.max_score = self.score
             else:
                 # Screen
                 x = output[i]
@@ -71,7 +76,11 @@ class ArcadeCabinet(object):
         return output[(i+3):]
 
     def _print_screen(self, output: List[int]):
-        """ Creates a heatmap to represent the current screen status """
+        """ Creates a heatmap to represent the current screen status,
+        or print the status in the terminal, depending on the value of
+        the parameter 'console' """
+        if not self.print_screen:
+            return
         screen = self.screen.transpose().toarray()
 
         # Return a heatmap (for part 1)
@@ -83,34 +92,36 @@ class ArcadeCabinet(object):
             return ax.pcolor(screen)
 
         # Print the current screen in the console
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
         for row in screen:
             line = []
             for i in range(len(row)):
                 tile = row[i]
-                if tile==self.tiles['wall']:
-                    if i==0 or i==(self.x_lim-1):
+                if tile == self.tiles['wall']:
+                    if i == 0 or i == (self.x_lim-1):
                         line.append('|')
                     else:
                         line.append('-')
-                elif tile==self.tiles['block']:
+                elif tile == self.tiles['block']:
                     line.append('#')
-                elif tile==self.tiles['paddle']:
+                elif tile == self.tiles['paddle']:
                     line.append('=')
-                elif tile==self.tiles['ball']:
+                elif tile == self.tiles['ball']:
                     line.append('o')
                 else:
                     line.append(' ')
             print(''.join(line))
-        
+
         # Print the current score
+        print("\n")
         print(f'Your score: {self.score}')
 
     def _initial_screen(self):
         """ Print initial screen before user starts playing """
 
-         # Get all the movements
+        # Get all the movements
         output = []
-        while not self.computer.waiting_for_input:
+        while not self.computer.waiting_for_input and not self.computer.execution_finished:
             output.extend(self.computer.get_output())
 
         # Get size of the screen
@@ -126,6 +137,7 @@ class ArcadeCabinet(object):
 
     @staticmethod
     def _read_key():
+        """ Reads an arrow from the keyboard. Up and down are neutral movements """
         keypress = readchar.readkey()
         key = None
         while key is None:
@@ -147,9 +159,9 @@ class ArcadeCabinet(object):
         if not self.autoplay:
             return self._read_key()
         else:
-            ball = (self.screen==self.tiles['ball']).nonzero()
-            paddle = (self.screen==self.tiles['paddle']).nonzero()
-            if len(ball[0])>0 and len(paddle[0])>0:
+            ball = (self.screen == self.tiles['ball']).nonzero()
+            paddle = (self.screen == self.tiles['paddle']).nonzero()
+            if len(ball[0]) > 0 and len(paddle[0]) > 0:
                 if ball[0][0] < paddle[0][0]:
                     return 'left'
                 elif ball[0][0] > paddle[0][0]:
@@ -160,7 +172,7 @@ class ArcadeCabinet(object):
                 return 'neutral'
 
     def start_game(self):
-        """ Runs the full program """
+        """ Runs the full game """
         self.computer.run_program()
 
         # Print initial screen
@@ -170,14 +182,14 @@ class ArcadeCabinet(object):
 
         leftovers = []
         while not self.computer.execution_finished:
-            if self.autoplay is True:
+            if self.autoplay is True and self.print_screen is True:
                 time.sleep(0.2)
 
             if self.computer.waiting_for_input:
                 key = self._get_key()
-                if key=='left':
+                if key == 'left':
                     self.computer.set_input(-1, clear=True)
-                elif key=='right':
+                elif key == 'right':
                     self.computer.set_input(1, clear=True)
                 else:
                     self.computer.set_input(0, clear=True)
@@ -188,7 +200,7 @@ class ArcadeCabinet(object):
             leftovers = self._update_status(output)
 
             # Sometimes the output is not complete...
-            while len(leftovers)>0:
+            while len(leftovers) > 0:
                 output = leftovers + self.computer.get_output()
                 leftovers = self._update_status(output)
             self._print_screen(output)
@@ -203,16 +215,42 @@ class ArcadeCabinet(object):
         return (self.screen == tile).sum()
 
 
-def play_arcade(autoplay: bool = False):
+def play_arcade(autoplay: bool = False, print_screen: bool = True):
+    """Runs the game!
+    
+    Keyword Arguments:
+        autoplay {bool} -- Whether the computer should play (default: {False})
+        print_screen {bool} -- In case the computer is playing, whether the
+            screen should be printed or not (default: {True})
+    """
     arcade = ArcadeCabinet(program_path='program.txt',
                            root_path='data/raw/day13',
-                           console=True,
-                           autoplay=autoplay)
+                           autoplay=autoplay,
+                           print_screen=print_screen
+                           )
 
-    print("Let's play!")
+    if autoplay is False:
+        print("\n\n")
+        print("---------------------------------------------------")
+        print("- Use left and right arrows to move the paddle")
+        print("- Use up or down arrow to stay in the same position")
+        print("---------------------------------------------------")
+        print("\n\n")
+        print("Let's play! (press any key to start)")
+        while readchar.readkey() is None:
+            pass
+
     arcade.insert_quarters(2)
     arcade.start_game()
-    
+
+    print("\n\n")
+    if arcade.score == 0:
+        print('GAME OVER...')
+    else:
+        print('WELL DONE!!!')
+    print(f' Your final score is: {arcade.max_score}')
+    print("\n\n")
+
 
 if __name__ == '__main__':
     play_arcade()
